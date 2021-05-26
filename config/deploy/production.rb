4e3a24a2514cacdf805a6b2715a9e2df29e9b99c
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-05-26 12:29:56 +0200

    update ssh config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,12 +41,12 @@ server "vm-green.multimediatechnology.at", user: "deploy", roles: %w{app db web}
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/izzyu20/.ssh/id_rsa),
-#    forward_agent: true,
-#    port: 5412
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/home/izzyu20/.ssh/ssh-ed25519),
+   forward_agent: true,
+   port: 5412
+   #auth_methods: %w(password)
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
