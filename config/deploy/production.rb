2fd0518ee7b87719eb8e670063355d4f190dbf94
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-05-26 12:21:42 +0200

    add ssh config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,11 +41,11 @@ server "vm-green.multimediatechnology.at", user: "deploy", roles: %w{app db web}
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/home/izzyu20/.ssh/id_rsa),
+   forward_agent: true,
+   #auth_methods: %w(password)
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
